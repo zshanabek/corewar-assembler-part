c0a9cb1e08d0c18094296be4e80bca0468f53bb5
Zhunissali <zshanabek@gmail.com>
2018-07-22 15:29:45 +0300

    [Refactor]

--- start.c
+++ start.c
@@ -1,15 +1,14 @@
 #include "asm.h"
 
-void	get_name(char *line, int *flag, int fd2)
+void	get_name(char *line, int fd2)
 {
-	int			i;
 	long		magic;
 	char		*name;
 	char		**arr;
 
-	if (*flag == 0 && ft_strstr(line, NAME_CMD_STRING))
+	if (ft_strstr(line, NAME_CMD_STRING))
 	{
-		i = 0;		
+
 		arr = ft_strsplit(line, '"');
 		if (ft_2darrlen(arr) == 2 && ft_strequ(ft_strtrim(arr[0]), NAME_CMD_STRING))
 		{
@@ -21,20 +20,16 @@ void	get_name(char *line, int *flag, int fd2)
 			write(fd2, name, PROG_NAME_LENGTH);
 		}
 	}
-	*flag = 1;	
 }
 
-void	get_comment(char *line, int *flag, int fd2)
+void	get_comment(char *line, int fd2)
 {
-	int			i;
 	char		*comment;
 	char		**arr;
 
-	if (*flag == 1 && ft_strstr(line, COMMENT_CMD_STRING))
+	if (ft_strstr(line, COMMENT_CMD_STRING))
 	{
-		i = 0;		
 		arr = ft_strsplit(line, '"');
-		ft_printf("ok: %d\n", ft_2darrlen(arr));		
 		if (ft_2darrlen(arr) == 2 && ft_strequ(ft_strtrim(arr[0]), COMMENT_CMD_STRING))
 		{
 			comment = ft_strnew(COMMENT_LENGTH);
@@ -43,7 +38,6 @@ void	get_comment(char *line, int *flag, int fd2)
 			write(fd2, comment, COMMENT_LENGTH);
 		}
 	}
-	*flag = 2;			
 }
 
 int main(int ac, char **av)
@@ -51,7 +45,6 @@ int main(int ac, char **av)
 	int		fd;
 	int		fd2;
 	char	*line;
-	int		i;
 
 	line = NULL;
 	if (ac != 2)
@@ -63,11 +56,9 @@ int main(int ac, char **av)
 	fd2 = open("mbappe.cor", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		exit(1);
-	i = 0;
-	while (get_next_line(fd, &line))
-	{
-		get_name(line, &i, fd2);
-		get_comment(line, &i, fd2);
-		free(line);
-	}
+	get_next_line(fd, &line);
+	get_name(line, fd2);
+	get_next_line(fd, &line);	
+	get_comment(line, fd2);	
+	
 }
