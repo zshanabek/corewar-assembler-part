62b8bfdf1b096f6f342dcb5d1931b6c9447f06dc
Zhunissali <zshanabek@gmail.com>
2018-07-22 12:25:27 +0300

    [Refactor]

--- op.h
+++ op.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: zshanabe <zshanabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2013/11/06 14:21:46 by zaz              ###   ########.fr       */
+/*   Updated: 2018/07/22 11:36:44 by zshanabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 ** Toutes les tailles sont en octets.
 ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
 */
+#ifndef OP_H
+#define OP_H
 
 #define IND_SIZE				2
 #define REG_SIZE				4
@@ -74,3 +76,5 @@ typedef struct		header_s
   unsigned int		prog_size;
   char				comment[COMMENT_LENGTH + 1];
 }					header_t;
+
+#endif
--- start.c
+++ start.c
@@ -1,18 +1,17 @@
 #include "asm.h"
 
-void	get_name(char *line, int fd2)
+void	get_name(char *line, int *flag, int fd2)
 {
 	int			i;
 	long		magic;
 	char		*name;
 	char		**arr;
-	static int	flag;
 
-	if (flag == 0 && ft_strstr(line, NAME_CMD_STRING))
+	if (*flag == 0 && ft_strstr(line, NAME_CMD_STRING))
 	{
 		i = 0;		
 		arr = ft_strsplit(line, '"');
-		if (ft_strequ(ft_strtrim(arr[0]), NAME_CMD_STRING))
+		if (ft_2darrlen(arr) == 2 && ft_strequ(ft_strtrim(arr[0]), NAME_CMD_STRING))
 		{
 			magic = COREWAR_EXEC_MAGIC;
 			write(fd2, &magic, 4);
@@ -20,40 +19,39 @@ void	get_name(char *line, int fd2)
 			ft_bzero(name, PROG_NAME_LENGTH);
 			ft_strcpy(name, arr[1]);
 			write(fd2, name, PROG_NAME_LENGTH);
-			flag = 1;
 		}
 	}
+	*flag = 1;	
 }
 
-void	get_comment(char *line, int fd2)
+void	get_comment(char *line, int *flag, int fd2)
 {
 	int			i;
 	char		*comment;
 	char		**arr;
-	static int	flag;
 
-	if (flag == 0 && ft_strstr(line, COMMENT_CMD_STRING))
+	if (*flag == 1 && ft_strstr(line, COMMENT_CMD_STRING))
 	{
 		i = 0;		
 		arr = ft_strsplit(line, '"');
-		if (ft_strequ(ft_strtrim(arr[0]), COMMENT_CMD_STRING))
+		ft_printf("ok: %d\n", ft_2darrlen(arr));		
+		if (ft_2darrlen(arr) == 2 && ft_strequ(ft_strtrim(arr[0]), COMMENT_CMD_STRING))
 		{
 			comment = ft_strnew(COMMENT_LENGTH);
 			ft_bzero(comment, COMMENT_LENGTH);
 			ft_strcpy(comment, arr[1]);
 			write(fd2, comment, COMMENT_LENGTH);
-			flag = 1;
 		}
 	}
+	*flag = 2;			
 }
-
 
 int main(int ac, char **av)
 {
-	char *line;
-	char ch;
-	int fd;
-	int fd2;
+	int		fd;
+	int		fd2;
+	char	*line;
+	int		i;
 
 	line = NULL;
 	if (ac != 2)
@@ -63,10 +61,13 @@ int main(int ac, char **av)
 	}
 	fd = open(av[1], O_RDONLY);
 	fd2 = open("mbappe.cor", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		exit(1);
+	i = 0;
 	while (get_next_line(fd, &line))
 	{
-		get_name(line, fd2);
-		get_comment(line, fd2);
+		get_name(line, &i, fd2);
+		get_comment(line, &i, fd2);
 		free(line);
 	}
 }
