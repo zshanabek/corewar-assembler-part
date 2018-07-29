1b061a8998ad0d11c8f3407c858ec4783ecf7868
Zhunissali <zshanabek@gmail.com>
2018-07-29 18:09:01 +0300

    [Fix] - parse with empty lines

--- parse.c
+++ parse.c
@@ -83,17 +83,16 @@ void	get_opcode(t_opcode *opcode, int h, int i, char *line)
 	get_params(opcode, i, line);
 }
 
-t_label	*parse_instr(t_opcode **ohead, char *line)
+void	parse_instr(t_opcode **ohead, t_label **lhead, char *line)
 {
 	int			i;
 	int			h;
-	t_label		*label;
 	t_opcode	*opcode;
 
-	ft_printf("%s\n", line);
 	i = 0;
-	label = NULL;
 	opcode = create_opcode();
+	opcode->label = *lhead;
+	*lhead = NULL;
 	ft_lstaddendopcode(ohead, opcode);
 	while (line[i] && ft_isws(line[i]))
 		i++;
@@ -102,9 +101,6 @@ t_label	*parse_instr(t_opcode **ohead, char *line)
 		i++;
 	if (line[i] == LABEL_CHAR)
 	{
-		label = create_label();
-		label->name = ft_strsub(line, h, i-h);
-		ft_lstaddendlabel(&opcode->label, label);
 		i++;
 		while (line[i] && ft_isws(line[i]))
 			i++;
@@ -114,23 +110,55 @@ t_label	*parse_instr(t_opcode **ohead, char *line)
 		get_opcode(opcode, h, i, line);
 	}
 	else
-		get_opcode(opcode, h, i, line);
-	return (label);
+		get_opcode(opcode, h, i, line);		
+}	
+
+int	get_label(t_label **lhead, char *line)
+{
+	t_label		*item;
+	int			i;
+	int			h;	
+
+	i = 0;
+	while (line[i] && ft_isws(line[i]))
+		i++;
+	h = i;
+	while (is_label_char(line[i]) && line[i] != LABEL_CHAR)
+		i++;
+	if (line[i] == LABEL_CHAR)
+	{
+		item = create_label();
+		item->name = ft_strsub(line, h, i-h);
+		ft_lstaddendlabel(lhead, item);
+	}
+	else
+		return (1);
+	i++;
+	while (line[i] && ft_isws(line[i]))
+		i++;
+	if (line[i] == '\0')
+		return (0);
+	return (1);
 }
+
 
 void	read_instr(int fd, char *line)
 {
 	t_opcode	*ohead;
+	t_label 	*lhead;
 
 	ohead = NULL;
+	lhead = NULL;
 	while (get_next_line(fd, &line))
 	{
-		if (line[0] != '\0' && line[0] != COMMENT_CHAR)
+		if (line[0] != '\0' && line[0] != COMMENT_CHAR && !ft_isempty(line))
 		{
 			clear_comment(line);
-			parse_instr(&ohead, line);
+			if (get_label(&lhead, line))
+				parse_instr(&ohead, &lhead, line);
 		}
 	}
 	iter_opcode(ohead, print_opcode);
+	// iter_label(lhead, print_label);
 }
 
