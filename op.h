d46ef1582816b40ecd961950feb173a59dedaec8
Vadim Radchenko <radchenko.v@ukr.net>
2018-08-03 16:02:09 +0300

    norme

--- op.h
+++ op.h
@@ -60,9 +60,9 @@ typedef char	t_arg_type;
 typedef struct		header_s
 {
   unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
+  char				p[PROG_NAME_LENGTH + 1];
   unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
+  char				c[COMMENT_LENGTH + 1];
 }					header_t;
 
 typedef	struct	s_op
--- start.c
+++ start.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   start.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vradchen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/03 15:37:37 by vradchen          #+#    #+#             */
+/*   Updated: 2018/08/03 15:37:39 by vradchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "asm.h"
 
-static void	write_magic(int fd)
+static void		write_magic(int fd)
 {
 	long	magic;
-	
+
 	magic = COREWAR_EXEC_MAGIC;// 0xea83f3
 	magic = ((magic >> 24) & 0xff) | ((magic << 8) & 0xff0000) |
 		((magic >> 8) & 0xff00) | ((magic << 24) & 0xff000000);
@@ -13,7 +25,7 @@ static void	write_magic(int fd)
 	write(fd, &magic, 4);
 }
 
-void	ft_line_end(char *s, int i)
+void				ft_line_end(char *s, int i)
 {
 	clear_comment(s + i);
 	while (s[i])
@@ -25,12 +37,12 @@ void	ft_line_end(char *s, int i)
 	return (ft_strdel(&s));
 }
 
-void	ft_fill_info(char *answer, int fd, char *str, int max)
+static void		ft_f(char *answer, int fd, char *str, int max)
 {
-	int		i;
-	int		len;
-	char	*s;
-	int 	x;
+	int				i;
+	int				len;
+	char			*s;
+	int				x;
 
 	i = -1;
 	len = -1;
@@ -55,47 +67,36 @@ void	ft_fill_info(char *answer, int fd, char *str, int max)
 	exit(ft_printf("Too big line\n"));
 }
 
-void	ft_read_header(header_t *h, int fd)
+void			ft_read_header(header_t *h, int fd)
 {
-	char	*str;
-	//char	*s;
-	int 	x;
+	char			*s;
+	int				x;
 
 	x = 0;
-	str = NULL;
+	s = NULL;
 	h->magic = COREWAR_EXEC_MAGIC;
-	while (ft_gnl(fd, &str))
+	while (ft_gnl(fd, &s))
 	{
-		if (!(*h->prog_name) && ft_strncmp(str, NAME_CMD_STRING, 4) == 0)
-		{
-			//s = ft_strtrim(str + 5);
-			ft_fill_info(h->prog_name, fd, ft_strtrim(str + 5), PROG_NAME_LENGTH);
-			//ft_strdel(&s);
-			x++;
-		}
-		else if (!(*h->comment) && ft_strncmp(str, COMMENT_CMD_STRING, 8) == 0)
-		{
-			//s = ft_strtrim(str + 8);
-			ft_fill_info(h->comment, fd, ft_strtrim(str + 8), COMMENT_LENGTH);
-			//ft_strdel(&s);
-			x++;
-		}
-		else if (ft_strequ(str, ""))
+		if (!(*h->p) && ft_strncmp(s, NAME_CMD_STRING, 4) == 0 && x++ > -1)
+			ft_f(h->p, fd, ft_strtrim(s + 5), PROG_NAME_LENGTH);
+		else if (!(*h->c) && ft_strncmp(s, COMMENT_CMD_STRING, 8) == 0 && ++x > 0)
+			ft_f(h->c, fd, ft_strtrim(s + 8), COMMENT_LENGTH);
+		else if (ft_strequ(s, ""))
 		{
 		}
 		else
 		{
-			ft_strdel(&str);
+			ft_strdel(&s);
 			exit(ft_printf("No name or header.\n"));
 		}
 		if (x == 2)
-			return (ft_strdel(&str));
-		ft_strdel(&str);
+			return (ft_strdel(&s));
+		ft_strdel(&s);
 	}
 	exit(ft_printf("No name or header.\n"));
 }
 
-void	ft_bot_size(int fd2, t_opcode *ohead)
+void			ft_bot_size(int fd2, t_opcode *ohead)
 {
 	t_opcode		*i;
 	int				bot_size;
@@ -115,13 +116,27 @@ void	ft_bot_size(int fd2, t_opcode *ohead)
 	write(fd2, &len, 4);
 }
 
-int main(int ac, char **av)
+void			ft_main2(t_opcode *ohead, header_t *h)
 {
-	int			fd;
-	int			fd2;
-	char		*line;
-	header_t	*h;
-	t_opcode	*ohead;
+	int				fd2;
+
+	ft_hex(ohead);
+	fd2 = open("try.cor", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	write_magic(fd2);
+	write(fd2, &h->p, PROG_NAME_LENGTH + 4);
+	ft_bot_size(fd2, ohead);
+	write(fd2, &h->c, COMMENT_LENGTH + 4);
+	if (ohead != NULL)
+		ft_write_in(ohead, fd2);
+	system("leaks asm");
+}
+
+int				main(int ac, char **av)
+{
+	int				fd;
+	char			*line;
+	header_t		*h;
+	t_opcode		*ohead;
 
 	ohead = NULL;
 	line = NULL;
@@ -132,23 +147,12 @@ int main(int ac, char **av)
 	}
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-	{
-		ft_printf("Can't read source file %s\n", av[1]);
-		exit(1);
-	}
+		exit(ft_printf("Can't read source file %s\n", av[1]));
 	h = malloc(sizeof(header_t));
 	ft_read_header(h, fd);
 	read_instr(fd, line, &ohead);
 	if (!detect_blank_line(fd))
 		show_error();
 	iter_opcode(ohead, print_opcode);
-	ft_hex(ohead);
-	fd2 = open("try.cor", O_WRONLY | O_CREAT | O_TRUNC, 0644);	
-	write_magic(fd2);
-	write(fd2, &h->prog_name, PROG_NAME_LENGTH + 4);
-	ft_bot_size(fd2, ohead);
-	write(fd2, &h->comment, COMMENT_LENGTH + 4);
-	if (ohead != NULL)
-		ft_write_in(ohead, fd2);
-	system("leaks asm");
+	ft_main2(ohead, h);
 }
