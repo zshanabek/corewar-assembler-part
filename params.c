ec15f953414cf6f4c29735cd971d5742615ebcb3
Zhunissali <zshanabek@gmail.com>
2018-08-05 16:56:02 +0300

    [Refactor] - error messages

--- params.c
+++ params.c
@@ -22,33 +22,35 @@ void	is_valid_param(t_op *elem, t_param *cur, int nb, char *name)
 			j++;
 		}
 		if (k == 0)
-			exit(ft_printf("Invalid parameter %d type %s for instruction \"%s\"\n", i, massiv[cur->type - 1], name));						
+			exit(ft_printf("Invalid param %d type %s for command %s\n", i, 
+			massiv[cur->type - 1], name));						
 		k = 0;
 		i++;
 		cur = cur->next;
 	}
 }
 
-void	analyze_type(t_param *item, char *temp, int type, int code)
+int		analyze_type(t_param *item, char *temp, int type, int code)
 {
 	if (is_digital(temp) && type == 1)
 		item->ival = ft_atoi(temp);
 	else if (type == 2)
 		item->sval = temp;
 	else
-		exit(ft_printf("Syntax error\n"));
+		return (0);
 	if (code == REG_CODE && (item->ival > REG_NUMBER || item->ival < 0))
-		exit(ft_printf("t_dir value must be between 0 and 16\n"));
+		return (0);	
 	item->type = code;
+	return (1);
 }
 
-void	analyze_param(t_param *item, char *str, int code, int type)
+int		analyze_param(t_param *item, char *str, int code, int type)
 {
 	char	*temp;
 
 	temp = NULL;
 	if (str[1] == '\0' && code != IND_CODE)
-		exit(ft_printf("Syntax error INSTRUCTION \"%s\"\n", str));
+		return (0);
 	if (code == DIR_CODE)
 	{
 		temp = ft_strsub(str, 1, ft_strlen(str) - 1);
@@ -59,13 +61,15 @@ void	analyze_param(t_param *item, char *str, int code, int type)
 		else if (ft_isdigit(str[0]) || (str[0] == '-' && ft_isdigit(str[1])))
         	type = 1;
 		else
-			exit(ft_printf("Invalid t_dir\n"));
+			return (0);
 	}
 	if (str[0] == LABEL_CHAR || code == REG_CODE)
 		temp = ft_strsub(str, 1, ft_strlen(str) - 1);
 	else
 		temp = str;
-	analyze_type(item, temp, type, code);
+	if (!analyze_type(item, temp, type, code))
+		return (0);
+	return (1);
 }
 
 char	**get_params_array(t_opcode *opcode, int i, int n, char *line)
@@ -87,27 +91,30 @@ char	**get_params_array(t_opcode *opcode, int i, int n, char *line)
 
 void	get_params(t_opcode *opcode, char **arr, int n)
 {
+	int 		e;	
 	int			k;
 	char 		*temp;
 	t_param		*item;
 
+	e = 1;
 	k = 0;
 	while (arr[k])
 	{
 		temp = ft_strtrim(arr[k]);
 		item = create_param();	
 		if (temp[0] == 'r')
-			analyze_param(item, temp, REG_CODE, 1);
+			e = analyze_param(item, temp, REG_CODE, 1);
 		else if (temp[0] == DIRECT_CHAR)
-			analyze_param(item, temp, DIR_CODE, 0);
+			e = analyze_param(item, temp, DIR_CODE, 0);
 		else if (ft_isdigit(temp[0]) || (temp[0] == '-' && ft_isdigit(temp[1])))
-			analyze_param(item, temp, IND_CODE, 1);
+			e = analyze_param(item, temp, IND_CODE, 1);
 		else if (temp[0] == LABEL_CHAR)
-			analyze_param(item, temp, IND_CODE, 2);
-		else
-			exit(ft_printf("Syntax error at [%03d] INSTRUCTION \"%s\"\n", n, temp));
+			e = analyze_param(item, temp, IND_CODE, 2);
 		ft_lstaddendpar(&opcode->param, item);
-		free(temp);
+		if (e == 0)
+			exit(ft_printf("Syntax error at [%03d] INSTRUCTION \"%s\"\n",
+			n, temp));
+		free(temp);		
 		k++;
 	}
 }
